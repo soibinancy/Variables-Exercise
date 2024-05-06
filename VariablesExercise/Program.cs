ebcacc3164a2fdaa1677ad953f975d7e16b2accc
Soibi Nancy <39niches@gmail.com>
2024-05-06 11:50:06 -0500

    Exercise completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace VariablesExercise
+﻿using System.Transactions;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Soibi";
+            int myAge = 36;
+            Double avg = 98.00;
+            decimal myWallet = 400.00m;
+            bool isStrong = true;
+            char initial = 'S';
+            
+            // Writing the variables to the console
+            Console.WriteLine($"Name: {myName}\nAge: {myAge}\nAverage: {avg}\nCurrency: {myWallet}\nStrong: {isStrong}\nInitial: {initial}");
+
         }
     }
 }
